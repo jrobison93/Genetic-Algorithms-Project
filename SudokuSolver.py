89874cc7f16711417a289e44af794d27fb2cce0c
John Robison <10904512@live.mercer.edu>
2016-01-31 14:18:48 -0500

    Added a function to get the fitness of a chromosome.

--- SudokuSolver.py
+++ SudokuSolver.py
@@ -8,6 +8,7 @@ Created on Sat Jan 30 18:02:47 2016
 
 class chromosome:
     genomes = []
+    fitness = 0
 
 
 def printBoard():
@@ -22,10 +23,69 @@ def printBoard():
 
         print()
 
+
+def assessFitness(chromo):
+    score = 32
+    temp_board = []
+    count = 0
+
+    for i in range(9):
+        for j in range(9):
+            if board[i][j] == '-':
+                temp_board[i][j] = chromo.genome[count]
+                count += 1
+
+    for i in range(9):
+        for j in range(1, 10):
+            if temp_board[i].count(j) != 1:
+                score -= 1
+                break
+
+    cols = []
+
+    for i in range(9):
+        cols.append([])
+        for j in range(9):
+            cols[i].append(temp_board[j][i])
+
+    for i in range(9):
+        for j in range(1, 10):
+            if cols[i].count(j) != 1:
+                score -= 1
+                break
+
+    squares = []
+
+    for i in range(3):
+        for j in range(3):
+            squares.append([])
+            for k in range(3):
+                for l in range(3):
+                    squares[j + 3 * i].append(temp_board[k + 3 * j][l + 3 * i])
+
+    for i in range(9):
+        for j in range(1, 10):
+            if squares[i].count(j) != 1:
+                score -= 1
+                break
+    
+    print(score)
+    chromo.fitness = score
+
+
 board = []
+blank_spaces = 0
+
 print("Reading file to get board:\n")
 f = open("board.txt", "r")
 for i in range(9):
     board.append(list(f.readline().strip()))
 
+for i in range(9):
+    for j in range(9):
+        if board[i][j] == '-':
+            blank_spaces += 1
+
 printBoard()
+
+
